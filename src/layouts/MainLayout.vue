8b830a033e03990076296cba73cbb633cd0a0fe5
Kevin Chandra <kacol.bot@gmail.com>
2020-08-26 01:53:30 +0700

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          Absensi Karyawan
+        </q-toolbar-title>
+
+        <div class="orientation-landscape">
+          {{ currentTime }}
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-footer elevated class="orientation-portrait">
+      <q-toolbar class="flex-center">
+        {{ currentTime }}
+      </q-toolbar>
+    </q-footer>
+
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import moment from 'moment';
+
+moment.locale('id');
+
+export default {
+  name: 'MainLayout',
+  data: function() {
+    return {
+      currentTime: ''
+    };
+  },
+  methods: {
+    getTime() {
+      this.currentTime = moment().format('dddd, D MMMM Y (HH:mm:ss)');
+    },
+    startTimer() {
+      setInterval(() => this.getTime(), 1000);
+    }
+  },
+  mounted() {
+    this.startTimer();
+  }
+};
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,122 @@
+<template>
+  <q-page>
+    <q-dialog v-model="card">
+      <q-card style="min-width: 300px">
+        <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg"/>
+
+        <q-card-section>
+          <q-btn
+            fab
+            color="primary"
+            icon="place"
+            class="absolute"
+            style="top: 0; right: 12px; transform: translateY(-50%);"
+          />
+
+          <div class="row no-wrap items-center">
+            <div class="col text-h6 ellipsis">
+              Cafe Basilico
+            </div>
+            <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+              <q-icon name="place"/>
+              250 ft
+            </div>
+          </div>
+        </q-card-section>
+
+        <q-card-actions>
+          <q-btn v-close-popup flat color="primary" label="Reserve"/>
+          <q-btn v-close-popup flat color="primary" round icon="event"/>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+    <div class="q-pa-md">
+      <div class="row">
+        <div class="col-12 col-sm-6 order-last order-sm-first">
+          <div class="q-pa-sm">
+            <q-card>
+              <q-card-section class="bg-grey-3">
+                <div class="text-h5">Off</div>
+              </q-card-section>
+
+              <q-card-section>
+                <div class="row">
+                  <div class="col-6">
+                    <div class="q-pa-sm">
+                      <q-card>
+                        <q-card-section>
+                          <div class="text-h6">
+                            Yeremia
+                          </div>
+                        </q-card-section>
+                      </q-card>
+                    </div>
+                  </div>
+                  <div class="col-6">
+                    <div class="q-pa-sm">
+                      <q-card>
+                        <q-card-section>
+                          <div class="text-h6">
+                            Yeremia
+                          </div>
+                        </q-card-section>
+                      </q-card>
+                    </div>
+                  </div>
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 order-first order-sm-last">
+          <div class="q-pa-sm">
+            <q-card>
+              <q-card-section class="bg-positive text-white">
+                <div class="text-h5">On Shift</div>
+              </q-card-section>
+
+              <q-card-section>
+                <div class="row">
+                  <div class="col-6">
+                    <div class="q-pa-sm">
+                      <q-card>
+                        <q-card-section>
+                          <div class="text-h6">
+                            Yeremia
+                          </div>
+                        </q-card-section>
+                      </q-card>
+                    </div>
+                  </div>
+                  <div class="col-6">
+                    <div class="q-pa-sm">
+                      <q-card>
+                        <q-card-section>
+                          <div class="text-h6">
+                            Yeremia
+                          </div>
+                        </q-card-section>
+                      </q-card>
+                    </div>
+                  </div>
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+
+  data: function() {
+    return {
+      card: true
+    };
+  }
+};
+</script>
